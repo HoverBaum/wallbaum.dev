197cfc04755e6530aceca37c54bffd0bfc488022
Hendrik <mail@hendrikwallbaum.de>
2020-05-09 23:30:30 +0200

    🚧 save react for reference.

---
+++ react-save/build/precache-manifest.f5a9ec21dbecec03cefaa16435c612bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502caae78e284af9e99cdc2722ab3213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2830ff24b03c46e3c653",
+    "url": "/static/js/2.3af9a762.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3af9a762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5cdf8ce4d7aed4356b4",
+    "url": "/static/js/main.863a7c15.chunk.js"
+  },
+  {
+    "revision": "40fc57c7043fb373c90f",
+    "url": "/static/js/runtime-main.ba5897f6.js"
+  }
+]);
